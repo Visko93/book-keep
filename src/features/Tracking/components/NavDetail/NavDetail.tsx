2ffa6caa5e269fc1e83ae3dbbac01fec0d98c0eb
Vinicius SKonicezny <vskonicezny@gmail.com>
2022-07-24 13:42:32 -0300

    feat: styles checkpoint-1, add minimum style for Nav components

--- src/features/Tracking/components/NavDetail/NavDetail.tsx
+++ src/features/Tracking/components/NavDetail/NavDetail.tsx
@@ -4,13 +4,13 @@ import styles from "./style.module.css";
 export function NavDetail({ children }: Props) {
   return (
     <div className={styles.container}>
-      <div className="detail">
-        <span className="icon">X</span>
-        <h4 className="value">64</h4>
+      <div className={styles.detail}>
+        <span className={styles.icon}>X</span>
+        <h4 className={styles.value}>64</h4>
       </div>
-      <div className="detail">
-        <span className="icon">X</span>
-        <h4 className="value">02</h4>
+      <div className={styles.detail}>
+        <span className={styles.icon}>X</span>
+        <h4 className={styles.value}>02</h4>
       </div>
     </div>
   );
